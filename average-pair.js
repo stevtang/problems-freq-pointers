c55d363394c7070aa1c35dea29dfbbd32aea1cd5
Ryan Nissen <ryankn852@gmail.com>
2022-01-14 18:15:05 -0600

    adding average-pair.js

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,39 @@
+"use strict";
+
+/**TODO ADD DOCSTRING */
+
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arrOfInts, average) {
+
+    //variable for the first index
+    //variable for the last index
+
+    let smallIndex = 0;
+    let bigIndex = arrOfInts.length - 1;
+
+    while ( smallIndex < bigIndex ){
+
+        let tempAverage = (arrOfInts[smallIndex] + arrOfInts[bigIndex]) / 2;
+
+        if (tempAverage === average ){
+            return true;
+        } else if (tempAverage > average){
+
+            bigIndex -= 1;
+
+        } else {
+
+            smallIndex += 1;
+
+        }
+
+    }
+ 
+    return false;
+
 }
+
+
+
+//inputs sorted array of integers, target average
+//
