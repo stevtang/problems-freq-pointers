7bfbe7ad79e81721bf068b48e6329246c38956a6
Ryan Nissen <ryankn852@gmail.com>
2022-01-14 18:27:28 -0600

    adding two-arry-object.js completed

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arrOfKeys, arrOfValues) {
+
+    const outputObject = {}
+
+    //loop through arrOfKeys. Add key and value. If value doesnt exist add null.
+
+    for (let i = 0; i < arrOfKeys.length; i++){
+        if (arrOfValues[i] === undefined) {
+            outputObject[arrOfKeys[i]] = null;
+        } else {
+            outputObject[arrOfKeys[i]] = arrOfValues[i];
+        }
+    }
+    return outputObject;
 }
+
+//two arrays ov varying lengths
+//first array keys
+//second is values
+// if not enough values, keys should have null
+//if there are not enough keys ignore of values
