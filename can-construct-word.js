e943eecac729ad8431a1544bde611e46c2006408
= <stevenmtang1@gmail.com>
2022-01-14 16:03:51 -0800

    added can-construct test files to index.html, completed can-construct problem

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,29 @@
+"use strict";
+
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+function canConstructWord(word, str) {
+    if(word.length === 0 ){return true;}
+    if(str.length === 0){return false;}
+    
+    
+    let wordObj = createFreqCounter(word);
+    let strObj = createFreqCounter(str);
+    for(let char in wordObj){
+        if(wordObj[char] > strObj[char]){
+            return false;
+        }
+
+    }
+    return true;
+
+}
+
+function createFreqCounter(string){
+    let freqs ={};
+    for(let char of string){
+        let charCount = freqs[char] || 0;
+        freqs[char] = charCount + 1;
+    }
+    return freqs;
 }
